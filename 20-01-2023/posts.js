41eafecc7a2adf1415b6f265db66a2d4938083b9
gretaTomasoni <greta51198@gmail.com>
2023-01-20 19:49:33 +0100

    Esercizio 20-01-2023

---
+++ 20-01-2023/posts.js
@@ -0,0 +1,57 @@
+const postForm = document.querySelector("#postForm");
+const inputTitle = document.querySelector("#title");
+const inputPrice = document.querySelector("#price");
+const inputCategory = document.querySelector("#category");
+const inputImage = document.querySelector("#image");
+const inputDescription = document.querySelector("#description");
+
+postForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const newObj = {
+    title: inputTitle.value,
+    price: parseInt(inputPrice.value),
+    description: inputDescription.value,
+    categoryId: parseInt(inputCategory.value),
+    images: [inputImage.value],
+  };
+  funzionePost(newObj);
+  alert("Elemento aggiunto correttamente");
+});
+
+const funzionePost = (objBody) => {
+  fetch("https://api.escuelajs.co/api/v1/products", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(objBody),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log("RISPOSTA POST: ", data))
+    .catch((e) => console.log("ERRORE: ", e));
+};
+
+// ! POST PER CREAZIONE CATEGORIA
+
+const nameCategory = document.querySelector("#nameCategory");
+const imageCategory = document.querySelector("#imageCategory");
+const buttonCategory = document.querySelector("#postFormCategory");
+
+buttonCategory.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const objCategory = {
+    name: nameCategory.value,
+    image: imageCategory.value,
+  };
+  creationCategory(objCategory);
+  console.log(objCategory);
+});
+
+const creationCategory = (objCategory) => {
+  fetch("https://api.escuelajs.co/api/v1/categories", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(objCategory),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log("RISPOSTA POST CATEGORIA: ", data))
+    .catch((e) => console.log("ERRORE: ", e));
+};
---
+++ 20-01-2023/script.js
@@ -0,0 +1,243 @@
+const productsElement = document.querySelector(".productsList");
+const loader = document.querySelector(".spinner");
+const cartBtn = document.querySelector(".cart_button");
+const tendina = document.querySelector(".tendina");
+const textEmptyCart = document.createElement("p");
+tendina.appendChild(textEmptyCart);
+const totalQty = document.createElement("h3");
+
+const cart = [];
+
+cartBtn.addEventListener("click", () => {
+  tendina.classList.toggle("show");
+  if (cart.length === 0) {
+    textEmptyCart.textContent = "Il tuo carrello è vuoto";
+  }
+});
+
+fetch("https://api.escuelajs.co/api/v1/products")
+  .then((res) => res.json())
+  .then((data) => {
+    const newData = data;
+    let listaProdotti = data;
+
+    // let listaProdotti = data;
+    // const newData = data.filter((item) => item.category.id === 1);
+    console.log(newData);
+    dataManupulation(newData, ".products");
+  })
+  .catch((error) => {
+    console.log("MIO ERRORE: ", error);
+  });
+
+const dataManupulation = (element, divId) => {
+  loader.style = "display: none";
+  element.forEach((item) => {
+    const cardContainer = document.createElement("div");
+    cardContainer.className = "card";
+
+    const imgElement = document.createElement("img");
+    imgElement.setAttribute("src", item.images);
+    imgElement.className = "img_card";
+
+    const titleElement = document.createElement("h1");
+    titleElement.className = "title_card";
+    titleElement.textContent = item.title.toUpperCase();
+    // console.log(item.title);
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.className = "description_card";
+    descriptionElement.textContent = item.description.slice(0, 30);
+    // console.log(item.description);
+
+    const priceElement = document.createElement("p");
+    priceElement.className = "price_card";
+    priceElement.textContent = "€ " + item.price;
+
+    const btnElement = document.createElement("button");
+    btnElement.className = "button";
+
+    const btnImgElement = document.createElement("img");
+    btnImgElement.className = "button_img";
+    btnImgElement.setAttribute("src", "./img/cart.png");
+
+    const btnTextElement = document.createElement("p");
+    btnTextElement.className = "button_text";
+    btnTextElement.textContent = "add to basket";
+
+    btnElement.addEventListener("click", () => {
+      cartPopulator(item);
+      console.log(cart);
+    });
+
+    btnElement.append(btnImgElement, btnTextElement);
+    cardContainer.append(
+      imgElement,
+      titleElement,
+      descriptionElement,
+      priceElement,
+      btnElement
+    );
+    productsElement.appendChild(cardContainer);
+  });
+};
+
+const cartCreation = () => {
+  // PER QUANTITA
+  sum();
+
+  tendina.innerHTML = "";
+  totalQty.className = "totalQty";
+
+  cart.forEach((item, index) => {
+    const cartEl = document.createElement("div");
+    cartEl.className = "cart_element";
+    cartEl.innerHTML = `<p>${item.title}</p><p class="cart_price">€ ${
+      item.price * item.qty
+    }</p><hr class="line">`;
+
+    const quantityEl = document.createElement("p");
+    quantityEl.className = "quantityEl";
+    quantityEl.textContent = item.qty;
+
+    const lessquantityEl = document.createElement("button");
+    lessquantityEl.className = "lessquantityEl";
+    lessquantityEl.textContent = "-";
+
+    const plusquantityEl = document.createElement("button");
+    plusquantityEl.className = "plusquantityEl";
+    plusquantityEl.textContent = "+";
+
+    const xEl = document.createElement("h3");
+    xEl.className = "x_button";
+    xEl.textContent = "X";
+
+    lessquantityEl.addEventListener("click", () => {
+      if (item.qty >= 1) {
+        item.qty--;
+        sum();
+        quantityEl.textContent = item.qty;
+        cartEl.innerHTML = `<p>${item.title}</p><p class="cart_price">€ ${
+          item.price * item.qty
+        }</p><hr class="line">`;
+      }
+
+      if (item.qty === 0) {
+        xEl.remove();
+        lessquantityEl.remove();
+        plusquantityEl.remove();
+        quantityEl.remove();
+        cartEl.remove();
+        cart.splice(index, 1);
+      }
+    });
+
+    plusquantityEl.addEventListener("click", () => {
+      if (item.qty > 0) {
+        item.qty++;
+        sum();
+        quantityEl.textContent = item.qty;
+        cartEl.innerHTML = `<p>${item.title}</p><p class="cart_price">€ ${
+          item.price * item.qty
+        }</p><hr class="line">`;
+      }
+    });
+
+    xEl.addEventListener("click", () => {
+      xEl.remove();
+      lessquantityEl.remove();
+      plusquantityEl.remove();
+      quantityEl.remove();
+      cartEl.remove();
+      cart.splice(index, 1);
+      sum();
+    });
+
+    tendina.appendChild(totalQty);
+
+    tendina.append(
+      xEl,
+      quantityEl,
+      lessquantityEl,
+      plusquantityEl,
+      cartEl,
+      totalQty,
+      textEmptyCart
+    );
+  });
+};
+
+const cartPopulator = (item) => {
+  const newCartObj = {
+    id: item.id,
+    title: item.title,
+    price: item.price,
+    qty: 1,
+  };
+
+  const search = cart.filter((element) => {
+    if (element.id === newCartObj.id) {
+      element.qty++;
+      return element;
+    }
+  });
+
+  if (search.length === 0) {
+    cart.push(newCartObj);
+  }
+
+  if (cart.length === 0) {
+    textEmptyCart.textContent = "Il tuo carrello è vuoto";
+  }
+  cartCreation();
+};
+
+const sum = () => {
+  let total = 0;
+  cart.forEach((item) => {
+    total = total + item.qty;
+  });
+  totalQty.textContent = "Totale prodotti: " + total;
+  if (total === 0) {
+    textEmptyCart.textContent = "Il tuo carrello è vuoto";
+    totalQty.textContent = "";
+  } else {
+    textEmptyCart.textContent = "";
+  }
+};
+
+// TODO: FILTRARE PER CATEGORIE
+
+const select = document.querySelector("#categorySelect");
+let listaProdotti;
+
+const metodoGetCat = () => {
+  fetch("https://api.escuelajs.co/api/v1/categories/")
+    .then((res) => res.json())
+    .then((data) => {
+      creazioneSelectCategories(data);
+    })
+    .catch((e) => console.log("Questo è il mio errore: ", e));
+};
+metodoGetCat();
+
+const creazioneSelectCategories = (arrayCat) => {
+  arrayCat.forEach((item) => {
+    const optionValue = document.createElement("option");
+    optionValue.textContent = item.name;
+    optionValue.setAttribute("value", item.id);
+    select.appendChild(optionValue);
+  });
+};
+
+const filterByCategory = (idCat) => {
+  const fiteredProducts = listaProdotti.filter(
+    (item) => item.category.id === parseInt(idCat)
+  );
+  dataManupulation(fiteredProducts, ".products");
+};
+
+select.addEventListener("change", (e) => {
+  console.log(select.value);
+  filterByCategory(select.value);
+});
